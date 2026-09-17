accf1f65b4d4b030514aad7589b8aaebfd33ae6c
marleneuwamahoro <marleneuwamahoro422@gmail.com>
2026-09-17 10:08:34 +0200

    add task detail endpoint"

--- backend/tasks/urls.py
+++ backend/tasks/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import TaskListCreateView
+from .views import TaskListCreateView, TaskDetailView
 
 
 urlpatterns = [
     path("", TaskListCreateView.as_view(), name="task-list-create"),
+    path("<int:pk>/", TaskDetailView.as_view(), name="task-detail"),
 ]
--- backend/tasks/views.py
+++ backend/tasks/views.py
@@ -6,3 +6,8 @@ from .serializers import TaskSerializer
 class TaskListCreateView(generics.ListCreateAPIView):
     queryset = Task.objects.all().order_by("-createdAt")
     serializer_class = TaskSerializer
+
+
+class TaskDetailView(generics.RetrieveAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
